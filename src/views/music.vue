3f1afb54195f22642e5c0548120d7db3a74ef1ba
陈佳威 <719816921@qq.com>
2019-11-27 11:34:27 +0800

    11.27

--- src/views/music.vue
+++ src/views/music.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="music">
-    <van-button class="next" @click="end()">最后上传按钮</van-button>
+    <!-- <van-button class="next" @click="end()">最后上传按钮</van-button>
 
-    <router-link to="/">返回首页</router-link>
+    <router-link to="/">返回首页</router-link> -->
 
-    <router-link to="/findMusic">findMusic</router-link>
+    <router-link to="/findMusic">去主页面</router-link>
+    <br>
+    <router-link to="/about">去about页面</router-link>
   </div>
 </template>
 
