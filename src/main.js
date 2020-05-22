7e7744cd8ef2836a3fef1e5a13a9eb450c70938b
陈佳威 <719816921@qq.com>
2020-05-22 17:36:24 +0800

    实现拖动提交

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueDragResize from 'vue-drag-resize' //缩放、拖拽
+Vue.component('vue-drag-resize', VueDragResize)
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,83 @@
 <template>
   <div class="Home">
+    <div class="img">
+      <div class="drag1"></div>
+      <vue-drag-resize
+        aspectRatio
+        parentLimitation
+        @dragging="dragging"
+        @dragstop="onDragstop"
+        @resizing="resizing"
+        @resizestop="onResizstop"
+        :sticks="['tm','bm','ml','mr']"
+        :parentW="1000"
+        :parentH="600"
+        :x="300"
+        :y="300"
+        :z="999"
+        class="drag"
+      ></vue-drag-resize>
+    </div>
   </div>
 </template>
 
 <script>
+import VueDragResize from "vue-drag-resize";
 export default {
   name: "Home",
   data() {
-    return {
-    };
+    return {};
   },
-  components: {},
+  components: {
+    VueDragResize
+  },
   watch: {},
   methods: {
+    dragging(e) {
+      // console.log(e);
+    },
+    onDragstop(e) {
+      console.log(e);
+    },
+    resizing(e) {
+      // console.log(e);
+    },
+    onResizstop(e) {
+      console.log(e);
+    }
   },
-  mounted() {
-  }
+  created() {},
+  mounted() {}
 };
 </script>
 
 <style scoped>
+.img {
+  width: 1000px;
+  height: 600px;
+  background: url("../assets/1.jpg");
+  background-size: cover;
+  margin: 3%;
+  position: relative;
+}
+.drag {
+  width: 100px;
+  height: 100px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background: url("../assets/code.jpg");
+  background-size: cover;
+  border-radius: 50%;
+}
+/* .drag1 {
+  width: 100px;
+  height: 100px;
+  position: absolute;
+  top: 49px;
+  left: 494px;
+  background: url("../assets/code.jpg");
+  background-size: cover;
+  border-radius: 50%;
+} */
 </style>
